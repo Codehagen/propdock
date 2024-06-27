997e761ecbf1801851d6f169bbb4c466af0f6a17
vsoraas <vegard.soraas@gmail.com>
2024-06-27 04:03:33 +0200

    API auth middleware

---
+++ apps/api/src/routes/internal/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { Context } from "hono";
+import { CustomContext } from "../../types";
+import { Env } from "../../env";
+import { verifyFrontend } from "../../auth/handler";
+
+const DEBUG: boolean = true; // NB! Change to false before committing.
+
+
+export default async function internalAuthMiddleware(
+    c: Context<{
+      Bindings: Env;
+      Variables: CustomContext;
+    }>,
+    next: any,
+  ) {
+    // Skip all checks for the test endpoint
+    if (c.req.path === '/api/test') {
+      if (DEBUG) { console.debug("Middleware debug - inserting test variable into request context") }
+      c.set("test", true)
+      return next();
+    }
+
+    const FEKey = c.req.header("x-fe-key");
+    if (DEBUG) { console.debug("Middleware debug - API key header:", FEKey) }
+
+    if (!FEKey) {
+        return c.json({ ok: false, message: "API key is required" }, 400);
+    }
+
+    const apiKeyVerified = await verifyFrontend(FEKey);
+    if (!apiKeyVerified) {
+        return c.json({ ok: false, message: "Invalid API key" }, 401)
+    }
+
+    return next();
+}
