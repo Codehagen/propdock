76b4d7ce0c9ce420336978758f829029b4a2de9f
vsoraas <vegard.soraas@gmail.com>
2024-06-27 04:04:55 +0200

    Hono instance for internal use

---
+++ apps/api/src/routes/internal/index.ts
@@ -0,0 +1,51 @@
+import { Hono } from "hono";
+import { Env } from "../../env";
+import { CustomContext } from "../../types";
+import internalAuthMiddleware from "../../routes/internal/authMiddleware";
+import { createAPIKey } from "../../auth/unkey";
+
+
+const internal = new Hono<{
+  Bindings: Env;
+  Variables: CustomContext;
+}>();
+
+internal.use(internalAuthMiddleware);
+
+// Routes
+internal.all('/test', (c) => {
+  return c.text('GET /api/internal/test')
+})
+
+// API key management
+internal.post('/workspace/api/create', async (c) => {
+  let workspaceId
+  let serviceName
+  let prefix
+  
+  try {
+    const body = await c.req.json()
+    workspaceId = body.workspace
+    serviceName = body.serviceName ? body.serviceName : ""
+    prefix      = body.prefix ? body.prefix : ""
+
+    if (!workspaceId) {
+      return c.json({ ok: false, message: "Supply an ID for the workspace" }, 400);
+    }
+  } catch(error) {
+    console.error(error)
+  }
+
+  try {
+    const res = await createAPIKey(workspaceId, serviceName, prefix)
+    if (res) {
+      return c.json({ ok: true, message: res }, 201);
+    }
+  } catch(error) {
+    console.error(error)
+  }
+
+  return c.json({ ok: false, message: "Something went wrong" }, 500);
+})
+
+export default internal
