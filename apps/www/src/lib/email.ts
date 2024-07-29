45292417b1f63138444a52140016053c5d3ff540
meglerhagen <Christer.hagen@gmail.com>
2024-07-29 15:30:37 +0200

    removed resend

--- apps/www/src/lib/email.ts
+++ apps/www/src/lib/email.ts
@@ -1,4 +1,9 @@
-import { env } from "@/env";
-import { Resend } from "resend";
+// import { env } from "@/env";
+// import { Resend } from "resend";
 
-export const resend = new Resend(env.RESEND_API_KEY);
+// export const resend = new Resend(env.RESEND_API_KEY);
+
+export const sendEmail = async () => {
+  console.log("Email sending functionality not implemented")
+  // Implement your email sending logic here in the future
+}
