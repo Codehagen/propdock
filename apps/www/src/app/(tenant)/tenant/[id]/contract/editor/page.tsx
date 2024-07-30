9a96f74340e1d013f80d5b90c938b3d31ee921b7
meglerhagen <Christer.hagen@gmail.com>
2024-07-30 21:30:37 +0200

    semi working react-to-pdf

--- apps/www/src/app/(tenant)/tenant/[id]/contract/editor/page.tsx
+++ apps/www/src/app/(tenant)/tenant/[id]/contract/editor/page.tsx
@@ -18,39 +18,6 @@ export default async function TenantEditPage({
 
   try {
     const tenantDetails = await getTenantDetails(tenantId)
-
-    // const missingFields: string[] = []
-    // if (!tenantDetails?.contacts || tenantDetails.contacts.length === 0) {
-    //   missingFields.push("Du må legge til kontaktperson")
-    // }
-    // if (!tenantDetails?.contracts || tenantDetails.contracts.length === 0) {
-    //   missingFields.push("Du må legge til kontrakt")
-    // }
-
-    // if (missingFields.length > 0) {
-    //   return (
-    //     <DashboardShell>
-    //       <DashboardHeader
-    //         heading="Kontrakter"
-    //         text="Du må fikse følgende før du kan lage kontrakt."
-    //       />
-    //       <EmptyPlaceholder>
-    //         <EmptyPlaceholder.Icon name="help" />
-    //         <EmptyPlaceholder.Title>Du mangler følgende</EmptyPlaceholder.Title>
-    //         <EmptyPlaceholder.Description>
-    //           <ul>
-    //             {missingFields.map((field, index) => (
-    //               <li className="font-bold" key={index}>
-    //                 {field}
-    //               </li>
-    //             ))}
-    //           </ul>
-    //         </EmptyPlaceholder.Description>
-    //       </EmptyPlaceholder>
-    //     </DashboardShell>
-    //   )
-    // }
-
     const contractContent = generateContractContent(tenantDetails)
 
     return (
--- apps/www/src/components/editor/TenantEditor.tsx
+++ apps/www/src/components/editor/TenantEditor.tsx
@@ -1,20 +1,37 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
+import { usePDF } from "react-to-pdf"
 
 import Editor from "@/components/editor/editor"
 
 const TenantEditor = ({ contractContent }) => {
   const [value, setValue] = useState(contractContent)
+  const [isExporting, setIsExporting] = useState(false)
+  const editorRef = useRef(null)
+
+  const { toPDF, targetRef } = usePDF({
+    filename: "document.pdf",
+    method: "save",
+  })
+
+  const exportToPDF = async () => {
+    setIsExporting(true)
+    toPDF()
+    setIsExporting(false)
+  }
 
   return (
     <div>
-      <Editor
-        content={value}
-        onChange={setValue}
-        placeholder="Write your post here..."
-      />
-      {/* <Tiptap /> */}
+      <button onClick={exportToPDF}>Export to PDF</button>
+      <div ref={targetRef}>
+        <Editor
+          content={value}
+          onChange={setValue}
+          placeholder="Write your post here..."
+          hideToolbar={isExporting}
+        />
+      </div>
     </div>
   )
 }
