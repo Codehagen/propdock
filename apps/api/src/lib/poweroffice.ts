fa4c6b6c387675bf6b2ee90052977c6b179beb10
vsoraas <vegard.soraas@gmail.com>
2024-08-01 19:11:13 +0200

    PowerOffice: Create Invoice POC

--- apps/api/src/lib/poweroffice.ts
+++ apps/api/src/lib/poweroffice.ts
@@ -23,13 +23,25 @@ async function superget(env: Env, url: string, workspaceId: string) {
 async function superpost(env: Env, url:string, workspaceId: string, data: any) {
     const poHeaders = await getRequestHeaders(env, workspaceId)
 
-    const response = await fetch(url, {
-        method: "GET",
-        headers: poHeaders,
-        body: data,
-    })
+    console.log("Superpost request headers:", poHeaders)
+
+    let response
+    try {
+        response = await fetch(url, {
+            method: "POST",
+            headers: poHeaders,
+            body: JSON.stringify(data),
+        })
+    } catch(error: any) {
+        console.error("Superpost error:", error)
+        return
+    }
+
+    console.debug("Request to URL:", url)
+    console.debug("Response:", response.ok, response.status)
 
     if (!response.ok) {
+        console.error(`Bad response while posting to ${url}: ${response.status} ${response.statusText} ${await response.text()}`)
         throw new Error(`Bad response while posting to ${url}: ${response.status} ${response.statusText}`)
     }
 
