a13b39f37f1258ce6715ee3dac90e6bb3448cf44
vsoraas <vegard.soraas@gmail.com>
2024-08-14 20:10:01 +0200

    Expanded DB utility functions for handling API keys to also include upserting to auto-handle cases where a user re-auths an integration

--- apps/api/src/models/workspace.ts
+++ apps/api/src/models/workspace.ts
@@ -11,9 +11,28 @@ async function saveAPIKey(db: PrismaClient, workspaceId: string, key: string, se
     });
 }
 
+async function upsertAPIKey(db: PrismaClient, workspaceId: string, key: string, serviceName: string): Promise<void> {
+    await db.wSApiKey.upsert({
+        where: {
+            workspaceId_serviceName: {
+                workspaceId: workspaceId,
+                serviceName: serviceName,
+            },
+        },
+        update: {
+            secret: key,
+        },
+        create: {
+            workspaceId: workspaceId,
+            serviceName: serviceName,
+            secret: key,
+        },
+    });
+}
+
 
 async function saveAccessToken(db: PrismaClient, workspaceId: string, token: string, expiry: any, serviceName: string): Promise<void> {
-    const validTo = new Date(Date.now() + (expiry - 10) * 1000); // Is expiry given in seconds or ms? Currently assuming seconds, and converting to ms
+    const validTo = new Date(Date.now() + (expiry - 10) * 1000);
 
     await db.workspaceAccessToken.create({
         data: {
@@ -25,7 +44,32 @@ async function saveAccessToken(db: PrismaClient, workspaceId: string, token: str
     });
 }
 
+async function upsertAccessToken(db: PrismaClient, workspaceId: string, token: string, expiry: any, serviceName: string): Promise<void> {
+    const validTo = new Date(Date.now() + (expiry - 10) * 1000);
+
+    await db.workspaceAccessToken.upsert({
+        where: {
+            workspaceId_serviceName: {
+                workspaceId: workspaceId,
+                serviceName: serviceName,
+            },
+        },
+        update: {
+            secret: token,
+            validTo: validTo,
+        },
+        create: {
+            workspaceId: workspaceId,
+            serviceName: serviceName,
+            secret: token,
+            validTo: validTo,
+        },
+    });
+}
+
 export {
     saveAPIKey,
-    saveAccessToken
+    saveAccessToken,
+    upsertAPIKey,
+    upsertAccessToken
 }
